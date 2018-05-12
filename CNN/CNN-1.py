c38dc33ba92508d8765a4fc4df3038cf0d954d59
wang198927 <wang198927@163.com>
2018-05-12 22:23:37 +0800

    CNN练习(cudnn版本出问题了，还没调通)

---
+++ CNN/CNN-1.py
@@ -0,0 +1,65 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+mnist = input_data.read_data_sets('D:\Projects\DL\TensorFlow-practice\data', one_hot=True)
+
+x = tf.placeholder("float", shape = [None, 28,28,1])
+y_ = tf.placeholder("float", shape = [None, 10])
+
+#卷积操作封一层，方便传参数
+def conv2d(X, W):
+    return tf.nn.conv2d(input=X, filter=W, strides=[1, 1, 1, 1], padding='SAME')
+#池化操作封一层，方便传参数
+def max_pool_2x2(X):
+    return tf.nn.max_pool(X, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+#第一个卷积池化层
+W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1))
+b_conv1 = tf.Variable(tf.constant(.1, shape = [32]))
+h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+#第二个卷积池化层
+W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))#上一层的深度为32
+b_conv2 = tf.Variable(tf.constant(.1, shape = [64]))
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+#全连接层
+W_fc1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024], stddev=0.1))
+b_fc1 = tf.Variable(tf.constant(.1, shape = [1024]))
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+#Dropout
+keep_prob = tf.placeholder("float")
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+#输出层
+W_fc2 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1))
+b_fc2 = tf.Variable(tf.constant(.1, shape = [10]))
+y = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y_, logits = y))
+trainStep = tf.train.AdamOptimizer().minimize(loss)
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+
+batchSize = 50
+for i in range(1000):
+    batch = mnist.train.next_batch(batchSize)
+    trainingInputs = batch[0].reshape([batchSize,28,28,1])
+    trainingLabels = batch[1]
+    if i%100 == 0:
+        trainAccuracy = accuracy.eval(session=sess, feed_dict={x:trainingInputs, y_: trainingLabels,keep_prob: 1.0})
+        print ("step %d, training accuracy %g"%(i, trainAccuracy))
+    trainStep.run(session=sess, feed_dict={x: trainingInputs, y_: trainingLabels, keep_prob: 0.8})
+
+#最终测试集
+batch = mnist.test.next_batch(1000)
+testAccuracy = sess.run(accuracy, feed_dict={x: batch[0], y: batch[1]})
+print ("test accuracy %g"%(testAccuracy))
